9c186b5f3c9192d720c4825ec7559593571a3f1d
Huarachi2002 <roberthuarachi27@gmail.com>
2023-06-09 19:27:40 -0400

    get and post validator

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,10 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Calificacion;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
     public function storeUser(Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:30',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'registro' => 'required|unique:users',
+            'numero_telefono' => 'required|max:8|unique:users',
+            'foto' => 'required',
+            'foto_horario' => 'required',
+        ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -19,9 +30,11 @@ class ApiController extends Controller
             'foto_horario' => $request->foto_horario,
         ]);
 
-        if(!$user){
-            return response()->json(['message' => 'Error al registrar usuario']);
-        }
+        // if(!$user){
+            
+        //     return response()->json(['message' => 'Error al registrar usuario']);
+            
+        // }
 
         return response()->json(['message' => 'Usuario registrado correctamente']);
     }
@@ -29,6 +42,11 @@ class ApiController extends Controller
 
 
     public function storeCalificacion(Request $request){
+        $this->validate($request, [
+            'id_calificante' => 'required',
+            'id_calificado' => 'required',
+            'puntaje' => 'required|numeric',
+        ]);
         $calificacion = Calificacion::create([
             'id_calificante' => $request->id_calificante,
             'id_calificado' => $request->id_calificado,
@@ -42,4 +60,27 @@ class ApiController extends Controller
         return response()->json(['message' => 'Calificacion registrada correctamente']);
     }
 
+
+
+    public function getUser(Request $request){
+
+        $user = User::where('registro', $request->registro)->first();
+        
+        if(!$user){
+            return response()->json([
+                'msg' => 'Usuario no registrado'
+            ]);
+        }
+
+        
+        if(Hash::check($request->password, $user->password)){
+            return response()->json([
+                'msg' => 'Usuario validado'
+            ]);
+        }else{
+            return response()->json([
+                'msg' => 'Contraseña incorrecta'
+            ]);
+        }
+    }
 }
